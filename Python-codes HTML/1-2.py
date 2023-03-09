d61bda6f25fb88920b206190ef292aa22113c4d9
preethamgshiva <118341405+preethamgshiva@users.noreply.github.com>
2023-03-09 20:56:21 +0530

    Delete Python-codes HTML directory

--- Python-codes HTML/1-2.py
+++
@@ -1,8 +0,0 @@
-#Area of Triangle
-
-height = float(input("Input Height of Triangle: "))
-base = float(input("Input Base of Triangle: "))
-
-#Area of Triangle = 0.5*base*height
-area = 0.5*base*height
-print("Area of Triangle is", area)
--- Python-codes HTML/4.py
+++
@@ -1,6 +0,0 @@
-#Circumference of circle
-
-radius = float(input("Radius of the Circle: "))
-cicumference = 2*3.14*radius
-
-print("Circumference of the circle is ",cicumference)
