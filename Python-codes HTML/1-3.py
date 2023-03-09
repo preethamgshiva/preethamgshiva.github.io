63108b565612882be3b06d110c2fe859d868a72c
preethamgshiva <118341405+preethamgshiva@users.noreply.github.com>
2023-03-10 00:05:24 +0530

    Add files via upload

---
+++ Python-codes HTML/1-3.py
@@ -0,0 +1,5 @@
+#conversion of temperature from centigrade to fahrenheit
+
+deg_C = float(input("Temperature in Centigrade(°C): "))
+conv = (deg_C*9/5)+32
+print("Temperature in Fahrenheit is", conv)
---
+++ Python-codes HTML/1-5.py
@@ -0,0 +1,9 @@
+#  Define multiple strings, concatenate all strings, replicate the concatenated string multiple times.
+
+a= input("String 1: ")
+b= input("String 2: ")
+
+concatenation = a+b
+print(concatenation)
+replication = concatenation*2
+print(replication)
